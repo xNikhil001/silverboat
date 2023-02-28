46776e494cc9dd551f3cc25a0163eb36b96ee0ab
xNikhil001 <naikn8113@gmail.com>
2023-02-28 13:09:56 +0530

    aligned footer to bottom

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer>
-      <p>Copyright@silverboat.com <span>{new Date().getFullYear()}</span></p>
+    <footer className="h-12 w-full bg-gray-100 flex justify-center items-center">
+      <p>
+        Copyright@silverboat.com <span>{new Date().getFullYear()}</span>
+      </p>
     </footer>
   );
 }
